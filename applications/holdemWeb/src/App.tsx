bf164014d80e8e4a9044f620f520caae166444c7
Piotr Bartnik <piotr.bartnik@hotmail.com>
2021-09-12 11:40:52 +0200

    feat: add native react app to applications

--- applications/holdemWeb/src/App.tsx
+++ applications/holdemWeb/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import { Holdem } from "holdem";
 
-function App() {
+export const App: React.FC = () => {
   return (
-    <div>Holdem Chips</div>
+    <div>
+      <Holdem />
+    </div>
   );
-}
-
-export default App;
+};
--- applications/holdemWeb/src/index.tsx
+++ applications/holdemWeb/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Holdem } from "holdem";
+import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Holdem />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );

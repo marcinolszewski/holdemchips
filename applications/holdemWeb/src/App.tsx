48d438b3e037055af877530ce6ad4888f6ca1fa0
Marcin Olszewski <marcin.olszewski@quantoo.pl>
2021-09-22 14:15:15 +0200

    chore: eslint

--- applications/holdemWeb/src/App.tsx
+++ applications/holdemWeb/src/App.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { Holdem } from "holdem";
+import React from 'react';
+import { Holdem } from 'holdem';
 
-export const App: React.FC = () => {
-  return (
-    <div>
-      You can check the holdem down below:
-      <Holdem />
-    </div>
-  );
-};
+export const App: React.FC = () => (
+  <div>
+    You can check the holdem down below:
+    <Holdem />
+  </div>
+);
